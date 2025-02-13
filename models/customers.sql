ad568e8818da8d14bfee8462aaa5b09b567c0af7
Joao Lourenco <lourenco.joao@siemens.com>
2025-02-13 12:06:03 +0000

    Lab1

--- models/customers.sql
+++ models/customers.sql
@@ -1,9 +1,7 @@
 with orders as (
-    select
-        id as order_id,
-        customer_id,
-        created_at as ordered_at
-    from raw.ecomm.orders
+  select
+      *
+  from {{ ref('stg_ecomm__orders') }}
 ), 
 
 customers as (
