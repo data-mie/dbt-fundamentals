5d76133b98f95d2c72fc6ca2963f9b2fc07f752d
Marcel Martins <marcel.marques_martins@siemens.com>
2025-02-13 12:23:12 +0000

    Lab 1 Completed

--- models/customers.sql
+++ models/customers.sql
@@ -1,45 +1,33 @@
-with orders as (
-    select
-        id as order_id,
-        customer_id,
-        created_at as ordered_at
-    from raw.ecomm.orders
-), 
+-- Select all columns from the staging model
+-- Note that no rename is needed here as the renaming was done in the staging model
+with
+    orders as (select * from {{ ref("orders") }}),
 
-customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name,
-        email,
-        address,
-        phone_number
-    from raw.ecomm.customers
-),
+    customers as (select * from {{ ref("stg_ecomm__customers") }}),
 
-customer_metrics as (
-    select
-        customer_id,
-        count(*) as count_orders,
-        min(ordered_at) as first_order_at,
-        max(ordered_at) as most_recent_order_at
-    from orders
-    group by 1
+    customer_metrics as (
+        select
+            customer_id,
+            count(*) as count_orders,
+            min(ordered_at) as first_order_at,
+            max(ordered_at) as most_recent_order_at,
+            avg(delivery_time_from_collection) as average_delivery_time_from_collection,
+            avg(delivery_time_from_order) as average_delivery_time_from_order
+        from orders
+        group by 1
 
-),
+    ),
 
-joined as (
-    select
-        customers.*,
-        coalesce(customer_metrics.count_orders,0) as count_orders,
-        customer_metrics.first_order_at,
-        customer_metrics.most_recent_order_at
-    from customers
-    left join customer_metrics on (
-        customers.customer_id = customer_metrics.customer_id
+    joined as (
+        select
+            customers.*,
+            coalesce(customer_metrics.count_orders, 0) as count_orders,
+            customer_metrics.first_order_at,
+            customer_metrics.most_recent_order_at
+        from customers
+        left join
+            customer_metrics on (customers.customer_id = customer_metrics.customer_id)
     )
-)
 
-select
-    *
+select *
 from joined
