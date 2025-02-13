8f53eca5146df37da04447112b48f6176d51c384
Tiago Enderenço <tiago.enderenco@siemens.com>
2025-02-13 16:29:39 +0000

    Lab 2 finished

---
+++ macros/parse_date.sql
@@ -0,0 +1,10 @@
+-- Parse dates
+{% macro parse_date(column) %}
+
+coalesce(
+    try_to_date({{ column }}, 'MM/DD/YY'),
+    try_to_date({{ column }}, 'MM/DD/YYYY'),
+    try_to_date({{ column }}, 'YYYY-MM-DD')
+)
+
+{% endmacro%}
