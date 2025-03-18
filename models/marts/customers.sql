ca7adce219d91b34c9b16c56b5ea4625b8a0de22
Simo Tumelius <simo@datamie.fi>
2025-03-18 09:08:11 +0000

    Lab 1 completed!

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,9 +1,7 @@
 with orders as (
     select
-        id as order_id,
-        customer_id,
-        created_at as ordered_at
-    from raw.ecomm.orders
+        *
+    from {{ ref('orders') }}
 ), 
 
 customers as (
@@ -14,7 +12,7 @@ customers as (
         email,
         address,
         phone_number
-    from raw.ecomm.customers
+    from {{ ref('stg_ecomm__customers') }}
 ),
 
 customer_metrics as (
@@ -22,7 +20,9 @@ customer_metrics as (
         customer_id,
         count(*) as count_orders,
         min(ordered_at) as first_order_at,
-        max(ordered_at) as most_recent_order_at
+        max(ordered_at) as most_recent_order_at,
+        avg(delivery_time_from_collection) as average_delivery_time_from_collection,
+        avg(delivery_time_from_order) as average_delivery_time_from_order
     from orders
     group by 1
 
@@ -33,7 +33,9 @@ joined as (
         customers.*,
         coalesce(customer_metrics.count_orders,0) as count_orders,
         customer_metrics.first_order_at,
-        customer_metrics.most_recent_order_at
+        customer_metrics.most_recent_order_at,
+        customer_metrics.average_delivery_time_from_collection,
+        customer_metrics.average_delivery_time_from_order
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
