fc98dff8464b50e5765a7140460a96708f801b59
Simo Tumelius <simo@datamie.fi>
2025-03-19 14:54:00 +0000

    Complete Lab 2!

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -6,13 +6,14 @@ with orders as (
 
 customers as (
     select
-        id as customer_id,
-        first_name,
-        last_name,
-        email,
-        address,
-        phone_number
+        *
     from {{ ref('stg_ecomm__customers') }}
+),
+
+survey_responses as (
+    select
+        *
+    from {{ ref('stg_sheets__customer_survey_responses') }}
 ),
 
 customer_metrics as (
@@ -22,7 +23,13 @@ customer_metrics as (
         min(ordered_at) as first_order_at,
         max(ordered_at) as most_recent_order_at,
         avg(delivery_time_from_collection) as average_delivery_time_from_collection,
-        avg(delivery_time_from_order) as average_delivery_time_from_order
+        avg(delivery_time_from_order) as average_delivery_time_from_order,
+        {% for days in [30,90,360] %}
+            count(case when ordered_at > current_date() - {{ days }} then 1 end) as count_orders_last_{{ days }}_days
+            {% if not loop.last %}
+                ,
+            {% endif %}
+        {% endfor %}
     from orders
     group by 1
 
@@ -31,14 +38,22 @@ customer_metrics as (
 joined as (
     select
         customers.*,
+        survey_responses.satisfaction_score,
+        survey_responses.survey_date,
         coalesce(customer_metrics.count_orders,0) as count_orders,
         customer_metrics.first_order_at,
         customer_metrics.most_recent_order_at,
         customer_metrics.average_delivery_time_from_collection,
-        customer_metrics.average_delivery_time_from_order
+        customer_metrics.average_delivery_time_from_order,
+        {%- for days in [30,90,360] %}
+            count_orders_last_{{ days }}_days,
+        {%- endfor %}
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
+    )
+    left join survey_responses on (
+        customers.email = survey_responses.customer_email
     )
 )
 
