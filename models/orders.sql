eec7c59761ab3a2a83d930cfa82398fed396a85f
Luis Henriques <luis.vicente-henriques@siemens.com>
2025-05-21 11:34:44 +0000

    added comments

--- models/orders.sql
+++ models/orders.sql
@@ -1,11 +1,22 @@
 with
-    orders as (select * from {{ ref("stg_ecomm__orders") }}),
+    orders as (
+        -- Select everything from stg_ecomm__orders
+        select * from {{ ref("stg_ecomm__orders") }}),
 
-    deliveries as (select * from {{ ref("stg_ecomm__deliveries") }}),
+    deliveries as (
+        -- Select everything from stg_ecomm__deliveries
+        select * from {{ ref("stg_ecomm__deliveries") }}),
 
-    deliveries_filtered as (select * from deliveries where status = 'delivered'),
+    deliveries_filtered as (
+          -- Select everything from deliveries
+  -- Include only deliveries with the 'delivered' status
+        select * from deliveries where status = 'delivered'),
 
     joined as (
+        -- Select order_id, customer_id, ordered_at, order_status, total_amount and store_id from orders
+        -- Calculate delivery_time_from_order as datediff (in minutes) between ordered_at (orders) and delivered_at (deliveries_filtered)
+        -- Calculate delivery_time_from_collection as datediff (in minutes) between picked_up_at (deliveries_filtered) and delivered_at (deliveries_filtered)
+        -- Do a left join between orders and deliveries_filtered using order_id
         select
             o.order_id,
             o.customer_id,
