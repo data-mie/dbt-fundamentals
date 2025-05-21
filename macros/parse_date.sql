de7e8bb492fbb75e284365fbccde714988dba0fd
Ceren İspir <ceren.ispir@siemens.com>
2025-05-21 15:23:37 +0000

    Bonus Lab 2

--- macros/parse_date.sql
+++ macros/parse_date.sql
@@ -1,7 +1,6 @@
-{% macro parse_date(date_column) %}
+{%- macro parse_date(date_column) -%}
     coalesce(
         try_to_date({{date_column}}, 'MM/DD/YY'),
         try_to_date({{date_column}}, 'MM/DD/YYYY'),
-        try_to_date({{date_column}}, 'YYYY-MM-DD')
-)
-{% endmacro %}
+        try_to_date({{date_column}}, 'YYYY-MM-DD'))
+{%- endmacro -%}
