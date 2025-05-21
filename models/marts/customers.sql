022e1f880d0de6a10599598bb75660a1debf6b2a
Benjamin Benthake <benjamin.benthake@siemens.com>
2025-05-21 15:22:41 +0000

    yehaaaa

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -13,6 +13,14 @@ customers as (
         address,
         phone_number
     from {{ ref('stg_ecomm__customers') }}
+
+),
+
+-- Add the survey_responses CTE
+survey_responses as (
+    select
+        *
+    from {{ ref('stg_sheets__customer_survey_responses') }}
 ),
 
 customer_metrics as (
@@ -22,7 +30,14 @@ customer_metrics as (
         min(ordered_at) as first_order_at,
         max(ordered_at) as most_recent_order_at,
         avg(delivery_time_from_collection) as average_delivery_time_from_collection,
-        avg(delivery_time_from_order) as average_delivery_time_from_order
+        avg(delivery_time_from_order) as average_delivery_time_from_order,
+        {% for days in [30,90,360] %}
+          count(case when ordered_at > current_date() - {{ days }} then 1 end) as count_orders_last_{{ days }}_days
+          {% if not loop.last %}
+        ,
+         {% endif %}
+{% endfor %}
+      
     from orders
     group by 1
 
@@ -35,10 +50,19 @@ joined as (
         customer_metrics.first_order_at,
         customer_metrics.most_recent_order_at,
         customer_metrics.average_delivery_time_from_collection,
-        customer_metrics.average_delivery_time_from_order
+        customer_metrics.average_delivery_time_from_order,
+         {% for days in [30,90,360] %}
+          count_orders_last_{{ days }}_days
+          {% if not loop.last %}
+        ,
+         {% endif %}
+{% endfor %}
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
+    )
+     left join survey_responses on (
+        customers.email = survey_responses.customer_email
     )
 )
 
---
+++ tests/count_orders_check.sql
@@ -0,0 +1,23 @@
+with orders as (
+    select
+        count(*) as orders_count
+    from {{ ref('orders') }}
+),
+
+customers as (
+    select
+        sum(count_orders_last_90_days) as customers_orders_last_90_days_count
+    from {{ ref('customers') }}
+),
+
+joined as (
+    select
+        *
+    from orders
+    cross join customers  -- The cross join is only one possible way
+    where customers_orders_last_90_days_count > orders_count
+)
+
+select
+    *
+from joined
