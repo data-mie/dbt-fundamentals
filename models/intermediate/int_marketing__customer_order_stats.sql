8bc3f61c883f6dd7a36f6a1ce40a4b62c3aceae8
Süheyla Seker <sueheyla.seker@siemens.com>
2025-05-22 09:32:14 +0000

    Lab 3 completed!

---
+++ models/intermediate/int_marketing__customer_order_stats.sql
@@ -0,0 +1,8 @@
+-- models/intermediate/marketing/int_marketing__customer_order_stats.sql
+select
+    customer_id,
+    round(avg(total_amount),2) as avg_order_amount,
+    count(*) as order_count
+from {{ ref('orders') }}
+where ordered_at > current_date - 180 -- Include orders from last 6 months
+group by 1
