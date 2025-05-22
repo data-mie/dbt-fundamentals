a9919bcb42e097f7a72de42e3c15c8fa8f28946f
Süheyla Seker <sueheyla.seker@siemens.com>
2025-05-22 13:41:58 +0000

    Congratulations on completing Lab 4!

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,8 +1,7 @@
 with orders as (
-    select
-        *
+    select *
     from {{ ref('orders') }}
-), 
+),
 
 customers as (
     select
@@ -16,8 +15,7 @@ customers as (
 ),
 
 survey_responses as (
-    select
-        *
+    select *
     from {{ ref('stg_sheets__customer_survey_responses') }}
 ),
 
@@ -27,14 +25,16 @@ customer_metrics as (
         count(*) as count_orders,
         min(ordered_at) as first_order_at,
         max(ordered_at) as most_recent_order_at,
-        avg(delivery_time_from_collection) as average_delivery_time_from_collection,
+        avg(delivery_time_from_collection)
+            as average_delivery_time_from_collection,
         avg(delivery_time_from_order) as average_delivery_time_from_order,
         {% for days in [30,90,360] %}
-        count(case when ordered_at > current_date() - {{ days }} then 1 end) as count_orders_last_{{ days }}_days
-        {% if not loop.last %}
-            ,
-        {% endif %}
-    {% endfor %}
+            count(case when ordered_at > current_date() - {{ days }} then 1 end)
+                as count_orders_last_{{ days }}_days
+            {% if not loop.last %}
+                ,
+            {% endif %}
+        {% endfor %}
     from orders
     group by 1
 ),
@@ -42,32 +42,31 @@ customer_metrics as (
 joined as (
     select
         customers.*,
-        coalesce(customer_metrics.count_orders,0) as count_orders,
+        coalesce(customer_metrics.count_orders, 0) as count_orders,
         customer_metrics.first_order_at,
         customer_metrics.most_recent_order_at,
         customer_metrics.average_delivery_time_from_collection,
         customer_metrics.average_delivery_time_from_order,
         {%- for days in [30,90,360] %}
-        customer_metrics.count_orders_last_{{ days }}_days
-         {% if not loop.last %}
-            ,
+            customer_metrics.count_orders_last_{{ days }}_days
+            {% if not loop.last %}
+                ,
             {% endif %}
         {%- endfor %}
     from customers
-    left join customer_metrics on (
-        customers.customer_id = customer_metrics.customer_id
-    )
+    left join customer_metrics
+        on (
+            customers.customer_id = customer_metrics.customer_id
+        )
     left join survey_responses on (
         customers.email = survey_responses.customer_email
     )
 ),
 
-final as(
-    select
-    *
+final as (
+    select *
     from joined
 )
 
-select
-*
+select *
 from final
