0d197630c14e70527eb9eaa2af6e7709e01c47b1
Jonas Grundler <jonas.grundler@siemens.com>
2025-05-22 13:38:32 +0000

    Completed lab 4!

--- models/marts/marketing/seven_week_active_customers.sql
+++ models/marts/marketing/seven_week_active_customers.sql
@@ -1,21 +1,15 @@
 -- Refactors
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(materialized="table") }}
 
 with
-    customer_metrics as (
-        select *
-        from {{ ref('int_marketing__customer_order_stats') }}
+customer_metrics as (
+    select * from {{ ref("int_marketing__customer_order_stats") }}
 
-    ),
+),
 
-    recent_customers as (
-        select *
-        from {{ ref('int_marketing__seven_week_active_customers') }}
-    )
+recent_customers as (
+    select * from {{ ref("int_marketing__seven_week_active_customers") }}
+)
 
 select
     customer_id,
@@ -23,6 +17,6 @@ select
     last_name,
     customer_metrics.avg_order_amount,
     customer_metrics.order_count
-from {{ ref("customers") }} as customers
+from {{ ref("customers") }}
 left join customer_metrics using (customer_id)
 inner join recent_customers using (customer_id)
